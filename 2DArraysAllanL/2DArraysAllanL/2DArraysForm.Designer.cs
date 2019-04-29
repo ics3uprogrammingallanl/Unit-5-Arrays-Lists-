13bd2158327b0f645b4a8d4da97da0e7f24c64b5
AL10120 <allan.leveille@ocsbstudent.ca>
2019-04-29 13:00:28 -0400

    Initial commit

---
+++ 2DArraysAllanL/2DArraysAllanL/2DArraysForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _2DArraysAllanL
+{
+    partial class frm2DArrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(252, 58);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(90, 34);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 2;
+            this.lblLength.Text = "Length";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(90, 85);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 4;
+            this.lblWidth.Text = "Width";
+            // 
+            // nudLength
+            // 
+            this.nudLength.Location = new System.Drawing.Point(148, 32);
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(57, 20);
+            this.nudLength.TabIndex = 6;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(148, 83);
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(57, 20);
+            this.nudWidth.TabIndex = 7;
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(12, 129);
+            this.txtArray.Multiline = true;
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(350, 151);
+            this.txtArray.TabIndex = 8;
+            // 
+            // frm2DArrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 292);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "frm2DArrays";
+            this.Text = "2D Arrays by AllanL";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.TextBox txtArray;
+    }
+}
+
---
+++ 2DArraysAllanL/2DArraysAllanL/2DArraysForm.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: AllanL
+ * Created on: April 6, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #38 - 2D Arrays
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DArraysAllanL
+{
+    public partial class frm2DArrays : Form
+    {
+        public frm2DArrays()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            int width;
+            int length;
+            Random randomNumberGenerator = new Random();
+            int aRandomNumber;
+            string aPieceOfText = null;
+
+            width = Convert.ToInt16(this.nudWidth.Value);
+            length = Convert.ToInt16(this.nudLength.Value);
+
+            int[,] a2DArray = new int[width, length];
+
+            for (int widthCounter = 0; widthCounter < width; widthCounter++)
+            {
+                for (int lengthCounter = 0; lengthCounter < length; lengthCounter++)
+                {
+                    aRandomNumber = randomNumberGenerator.Next(0, 9 + 1);
+                    a2DArray[widthCounter, lengthCounter] = aRandomNumber;
+                    aPieceOfText = aPieceOfText + " " + aRandomNumber;
+                }
+
+                aPieceOfText = aPieceOfText + "\r" + "\n";
+            }
+            this.txtArray.Text = aPieceOfText;
+        }
+    }
+}
